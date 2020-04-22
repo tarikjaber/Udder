98b6e7c08055b409b18defda3222b497ed0aed40
tarikjaber <55803936+tarikjaber@users.noreply.github.com>
2020-04-22 09:20:11 -0400

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Tarik Jaber on 4/22/20.
+//  Copyright © 2020 Tarik Jaber. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
